f026772f704323ab1ae6ad2a385e39b140a7f180
timonelmer <timon.elmer@gmail.com>
2016-08-23 16:06:37 +0200

    d

--- example_get.php
+++ example_get.php
@@ -1,12 +1,12 @@
 $fb = new Facebook\Facebook([
-  'app_id' => '{app-id}',
-  'app_secret' => '{app-secret}',
-  'default_graph_version' => 'v2.2',
+  'app_id' => '690212971127525',
+  'app_secret' => 'd42eda1ce1a1850a0d4f60ea9d45e0b5',
+  'default_graph_version' => 'v2.7',
   ]);
 
 try {
   // Returns a `Facebook\FacebookResponse` object
-  $response = $fb->get('/me?fields=id,name', '{access-token}');
+  $response = $fb->get('/me?fields=id,name', 'EAAJzvrr2ouUBAFOtXwuFfwGyVhE0FxZCyZCyYdozmohucZBqrfXaS8IcHsKY4ne3sUqioBPZAq896nLVKCJF4NPpKsEZCEMTonSMKZBheZA4E1HZCDBdMeMo5KR4ZBaTYLeOFYqi4YC5dtCMurbA2C1jBzSfXDG5HmQIKPeW9l6RDFwZDZD');
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
   echo 'Graph returned an error: ' . $e->getMessage();
   exit;
